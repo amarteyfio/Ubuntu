80aa97a305ce5296ecac96242dfe37a7edc11e7a
amarteyfio <amarteyfio>
2023-04-05 10:49:33 +0000

    initial commit

---
+++ app/database/db_cred.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Database credentials
+ *
+ * This script defines the database server, username, password, and name.
+*/
+
+// Define the database server.
+define('DB_SERVER', 'localhost');
+
+// Define the database username.
+define('DB_USERNAME', 'root');
+
+// Define the database password.
+define('DB_PASSWORD', '');
+
+// Define the database name.
+define('DB_NAME', 'ubuntu_db');
+
+?>
