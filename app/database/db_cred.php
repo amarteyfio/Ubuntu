e9da6ddb7a72dbc3c54227d8b1f48d52d9c95b7b
amarteyfio <amarteyfio>
2023-04-05 11:12:58 +0000

    fixes

--- app/database/db_cred.php
+++ app/database/db_cred.php
@@ -4,17 +4,24 @@
  *
  * This script defines the database server, username, password, and name.
 */
+define('URL',parse_url(getenv("CLEARDB_DATABASE_URL")));
 
 // Define the database server.
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', URL['host']);
 
 // Define the database username.
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', URL['user']);
 
 // Define the database password.
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', URL['pass']);
 
 // Define the database name.
-define('DB_NAME', 'ubuntu_db');
+define('DB_NAME', substr(URL['path'],1));
 
+$active_group = 'default';
+$query_builder = TRUE;
 ?>
+
+
+
+
